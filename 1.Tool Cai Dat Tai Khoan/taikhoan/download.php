c588fa52d7d4689e68d07aa9593bbe13daea4ca1
AnhNV <anhnv1310@gmail.com>
2022-05-18 17:20:06 +0700

    Sửa trang download.php

--- 1.Tool Cai Dat Tai Khoan/taikhoan/download.php
+++ 1.Tool Cai Dat Tai Khoan/taikhoan/download.php
@@ -2,22 +2,38 @@
 <html>
 <head>
 <title>Download Kiếm Thế Chí Tôn</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 <link href='http://kiemthechiton.net/favicon.ico' rel='icon' type='image/x-icon'/>
 </head>
 <body>
-<table width="100%" border="3" align="center" height="217" bordercolor=#606060 >
-         
 
-
-                <tr> 
-                  <td width="50%"> 
-                    <div align="center"><font face="Arial, Helvetica, sans-serif" color="blue">Link Google Drive:</font></div>
-                  </td>
-                  <td width="50%"> 
-                    <div align="center"><a href="http://www.fshare.vn/file/LQ1FN11V6R/" target=_blank><img src="http://1.bp.blogspot.com/-6f6JGWRowZo/Ugm5ameejeI/AAAAAAAABfA/zuoRwwRVITc/s1600/download1.png" width="129" height="41" border="0"></a></div>
-                  </td>
-                </tr>
-                
-              </table>
-	</body>
+	<div class="container">
+	  <h2>Tải game</h2>
+	  <p>Trường hợp đã có Client Kiếm Thế VNG, tải và giải nén tệp tin Patch vào thư mục chứa client Kiếm Thế.</p>            
+	  <table class="table table-bordered">
+		<thead>
+		  <tr>
+			<th>Tên tệp tin</th>
+			<th>Đường dẫn</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  <tr>
+			<td>Patch v1.0</td>
+			<td><a href="https://drive.google.com/file/d/10F4z538Vi16GaaRXB4H6jFmCY4JEu22a/view?usp=sharing" target=_blank>Link Google Drive</a></td>
+		  </tr>
+		  <tr>
+			<td>VPN</td>
+			<td><a href="https://drive.google.com/file/d/1wA1QtKr_FBb98F0Q8SxaBnAZ297T_bZD/view?usp=sharing" target=_blank>Link Google Drive</a></td>
+		  </tr>
+		</tbody>
+	  </table>
+	  <p>Trường hợp chưa có Client Kiếm Thế VNG. Bấm vào <a href="https://kiemthe.zing.vn/cam-nang/cai-dat-ngay/cai-dat-ngay.html" target=_blank>đây</a> để tải về client Kiếm Thế mới nhất.</p>
+	</div>
+</body>
 </html>
